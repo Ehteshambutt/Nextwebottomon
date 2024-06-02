746b1b51406e7416424b69c421570ade1b149292
Ehteshambutt <buttehtesham86@gmail.com>
2024-06-03 01:50:53 +0500

    save

--- src/app/NewPaymentCardSetup/page.js
+++ src/app/NewPaymentCardSetup/page.js
@@ -1,19 +1,29 @@
- 'use client';
- import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import PaymentForm from './paymentForm';
- 
-const stripePromise = loadStripe('pk_test_51OU7K2GzmgnXQM1ZzsvV9RUUBFbRKzol5julcMWC8zV8ckijoKAHbr1kBB2cwqbJuKN4kkxdomxe1fhpbNjkLDNm00DHUrBE3P');
+import React from 'react'
 
 const NewPaymentCardSetup = () => {
   return (
-    <div>
-      
-      <Elements stripe={stripePromise}>
-        <PaymentForm/>
-      </Elements>
-    </div>
-  );
-};
+    <div>NewPaymentCardSetup</div>
+  )
+}
 
-export default NewPaymentCardSetup;
+export default NewPaymentCardSetup
+
+//  'use client';
+//  import { loadStripe } from '@stripe/stripe-js';
+// import { Elements } from '@stripe/react-stripe-js';
+// import PaymentForm from './paymentForm';
+ 
+// const stripePromise = loadStripe('pk_test_51OU7K2GzmgnXQM1ZzsvV9RUUBFbRKzol5julcMWC8zV8ckijoKAHbr1kBB2cwqbJuKN4kkxdomxe1fhpbNjkLDNm00DHUrBE3P');
+
+// const NewPaymentCardSetup = () => {
+//   return (
+//     <div>
+      
+//       <Elements stripe={stripePromise}>
+//         <PaymentForm/>
+//       </Elements>
+//     </div>
+//   );
+// };
+
+// export default NewPaymentCardSetup;
